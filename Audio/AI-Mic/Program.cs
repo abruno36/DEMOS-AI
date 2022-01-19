47826f9a4c4c577c2915a1907e44f165da8f4bba
Bruno <pj_antoniobruno@bocombbm.com.br>
2022-01-19 16:01:56 -0300

    first commit - 19011601

---
+++ Audio/AI-Mic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace AI_Mic
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Falai garota(o)!");
+            var config = SpeechConfig.FromSubscription("67f92467f25c451f941ae8853536fe3f", "southcentralus");
+            var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+
+            var recognizer = new SpeechRecognizer(config, "pt-Br", audioConfig);
+
+            Console.WriteLine("Falei agora - pronto falei");
+
+            while (true)
+            {
+                var result = await recognizer.RecognizeOnceAsync();
+                var text = result.Text;
+                Console.WriteLine($"Rec: {text}");
+                if (text.ToLower().Contains("sair."))
+                {
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Translate/Translate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Translation;
+
+namespace Translate
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Falai garota(o)!");
+
+            var speechConfig = SpeechTranslationConfig.FromSubscription("67f92467f25c451f941ae8853536fe3f", "southcentralus");
+            speechConfig.SpeechRecognitionLanguage = "pt-BR";
+
+            //speechConfig.AddTargetLanguage("en-US");
+
+            var lang = new List<string>() { "en-US", "fr-FR", "es-AR" };
+            lang.ForEach(speechConfig.AddTargetLanguage);
+
+            var recognizer = new TranslationRecognizer(speechConfig);
+            
+            while (true)
+            {
+                var result = await recognizer.RecognizeOnceAsync();
+                if (result.Reason == ResultReason.TranslatedSpeech)
+                {
+                    foreach (var item in result.Translations)
+                    {
+                        Console.WriteLine($"{item.Key}: {item.Value}");
+                        if (item.Value == "Go out.")
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+         
+            Console.ReadLine();
+
+        }
+    }
+}
